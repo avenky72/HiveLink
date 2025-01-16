fa8d11e2fdcb37bb6a3dc4dbb14d458417c512e0
Atul Venkatesan <atulvenky@gmail.com>
2025-01-15 18:09:10 -0800

    navbar for mobile and desktop formatting with flex and gird

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import {
   ClerkProvider,
 } from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/theme-provider"
-
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,14 +21,26 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
       <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange>
-            {children}
+            <div className="min-h-screen">
+              <Navbar />
+
+              <main className="py-8">
+                {/* Container to center content */}
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">sidebar</div>
+                    <div className="lg:col-span-9">{children}</div>
+                  </div>
+                </div>
+              </main>
+            </div>
       </ThemeProvider>
         </body>
     </html>
